13d5b7c08d97cb0475a30723b6b158ce752886c2
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-04-09 17:16:00 +0300

    Dikstra Graph methods

--- GoT Route Finder/src/application/DijkstraGraphClass.java
+++ GoT Route Finder/src/application/DijkstraGraphClass.java
@@ -31,8 +31,10 @@ public class DijkstraGraphClass {
 		Set<NodeClass> unsettled = new HashSet<NodeClass>();
 		unsettled.add(from);
 		
+		//defined
 		Set<NodeClass> settled = new HashSet<NodeClass>();
 		
+		//undefined
 		while(unsettled.size() != 0)
 		{
 			NodeClass currentNode = getLowestUnsettledNode(unsettled, nodeDistanceMapping);
@@ -65,5 +67,63 @@ public class DijkstraGraphClass {
 		
 		Collections.reverse(route);
 		return route;
+	}
+	
+	private Map<Node, Node> getPreviousNodeMapping() {
+
+		Map<Node, Node> mappings = new HashMap<>();
+
+		for(Node n : getGraph().getNodes()) {
+
+			mappings.put(n, null);
+
+		}
+
+		return mappings;
+
+	}
+	
+	private Node getLowestUnsettledNode(Set<Node> unsettled, Map<Node, Integer> distanceMapping) {
+
+		Iterator<Node> iter = unsettled.iterator();
+
+		if (!iter.hasNext()) {
+
+			return null;
+
+		}
+
+		Node selected = iter.next();
+
+		while (iter.hasNext()) {
+
+			Node n = iter.next();
+
+			if (distanceMapping.get(n) < distanceMapping.get(selected)) {
+
+				selected = n;
+
+			}
+
+		}
+
+		return selected;
+
+	}
+	
+	private Map<Node, Integer> getNodeDistanceMapping(Node from) {
+
+		Map<Node, Integer> mappings = new HashMap<Node, Integer>();
+
+		for(Node n : getGraph().getNodes()) {
+
+			mappings.put(n, Integer.MAX_VALUE);
+
+		}
+
+		mappings.put(from, 0);
+
+		return mappings;
+
 	}
 }
