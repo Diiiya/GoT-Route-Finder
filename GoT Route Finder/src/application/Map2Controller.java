72774a01650dbcc605154131ea6ce12ff94d8ada
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-04-09 20:29:30 +0300

    Small changes

--- GoT Route Finder/src/application/Map2Controller.java
+++ GoT Route Finder/src/application/Map2Controller.java
@@ -3,15 +3,39 @@ package application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.MenuItem;
 
 public class Map2Controller {
 	
+	@FXML
+	private MenuItem fromA;
+	
+	@FXML
+	private MenuItem fromB;
+	
+	@FXML
+	private MenuItem fromC;
+	
+	@FXML
+	private MenuItem toA;
+	
+	@FXML
+	private MenuItem toB;
+	
+	@FXML
+	private MenuItem toC;
+	
+	@FXML
+	private Button findRouteButton;
+	
 
 	
 	@FXML
 	private void initialize() {
+		
+		
 
 	}
 	
