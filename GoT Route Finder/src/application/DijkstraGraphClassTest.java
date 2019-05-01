bdefea54b4d3792bf4e9039a39dd876800b208af
Izabela Kuzniar <iza8848@gmail.com>
2019-05-01 23:42:32 +0200

    deleted MyController, added test for safest path

--- GoT Route Finder/src/application/DijkstraGraphClassTest.java
+++ GoT Route Finder/src/application/DijkstraGraphClassTest.java
@@ -22,16 +22,16 @@ class DijkstraGraphClassTest {
 			nodesList.add(new NodeClass(Integer.toString(i), 100, 100));
 		}
 
-		nodesList.get(0).addTwoWayNeighbour(nodesList.get(1), 1, 1, 0);
-		nodesList.get(0).addTwoWayNeighbour(nodesList.get(2), 1, 3, 0);
-		nodesList.get(0).addTwoWayNeighbour(nodesList.get(3), 2, 3, 0);
+		nodesList.get(0).addTwoWayNeighbour(nodesList.get(1), 1, 1, 2);
+		nodesList.get(0).addTwoWayNeighbour(nodesList.get(2), 1, 3, 3);
+		nodesList.get(0).addTwoWayNeighbour(nodesList.get(3), 2, 3, 3);
 
-		nodesList.get(1).addTwoWayNeighbour(nodesList.get(2), 2, 1, 0);
-		nodesList.get(1).addTwoWayNeighbour(nodesList.get(3), 3, 3, 0);
+		nodesList.get(1).addTwoWayNeighbour(nodesList.get(2), 2, 1, 1);
+		nodesList.get(1).addTwoWayNeighbour(nodesList.get(3), 3, 3, 3);
 
-		nodesList.get(2).addTwoWayNeighbour(nodesList.get(4), 2, 1, 0);
+		nodesList.get(2).addTwoWayNeighbour(nodesList.get(4), 2, 1, 1);
 
-		nodesList.get(3).addTwoWayNeighbour(nodesList.get(4), 7, 3, 0);
+		nodesList.get(3).addTwoWayNeighbour(nodesList.get(4), 7, 3, 3);
 
 		this.graph = new GraphsClass(this.nodesList);
 
@@ -55,4 +55,11 @@ class DijkstraGraphClassTest {
 
 	}
 
+	@Test
+	void testSafestPath()
+	{
+		DijkstraGraphClass dGraph = new DijkstraGraphClass(this.graph);
+		List<NodeClass> path = dGraph.getPath(nodesList.get(0), nodesList.get(4), "safest");
+		assertEquals(3, path.size());
+	}
 }
